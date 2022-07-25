10c131f5ba39d006fb518624f522160daa5e4649
Dzenovich <zakirov.zrz@gmail.com>
2022-07-25 18:42:06 +0300

    added handler

---
+++ src/main/java/com/example/account/service/AccountCommandHandler.java
@@ -0,0 +1,33 @@
+package com.example.account.service;
+
+import com.example.account.exception.SumNotExistException;
+import com.example.account.model.command.ChangeAccount;
+import com.example.account.model.command.CheckAccount;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountCommandHandler {
+
+    private final EventSender eventSender;
+    private final AccountService accountService;
+
+    public AccountCommandHandler(EventSender eventSender, AccountService accountService) {
+        this.eventSender = eventSender;
+        this.accountService = accountService;
+    }
+
+    public void checkAccount(CheckAccount command) {
+        try {
+            accountService.createBalanceRequest(command.getRequestId(), command.getPersonId(), command.getSum());
+            eventSender.sendRequestConfirmed(command.getRequestId(), command.getSum());
+        } catch (SumNotExistException e) {
+            e.printStackTrace();
+            eventSender.sendRequestRejected(command.getRequestId(), command.getSum());
+        }
+    }
+
+    public void changeAccount(ChangeAccount command) {
+        accountService.executeBalanceRequest(command.getRequestId(), command.getPersonId(), command.getSum());
+        eventSender.sendAccountChanged(command.getRequestId(), command.getPersonId(), command.getSum());
+    }
+}
